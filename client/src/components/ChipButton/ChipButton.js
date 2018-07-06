e873dcbc7689412623feaf40da7caa125c46a6f9
Robbie Delfs <robbatron@gmail.com>
2018-07-05 20:41:24 -0700

    resized buttons and centered

--- client/src/components/ChipButton/ChipButton.js
+++ client/src/components/ChipButton/ChipButton.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import './ChipButton.css';
-import pokerChip from "./poker_chip.png"
+import React, { Component } from "react";
+import "./ChipButton.css";
+import pokerChip from "./poker_chip.png";
 
 class ChipButton extends Component {
-   render() {
-       return (
-        <div className = "chip-button">
-         <img src = { pokerChip } />
-         <div className = "button-text"> {this.props.text}
-         </div>
-        </div>    
-       );
-   } 
+  render() {
+    return (
+      <div className="chip-button">
+        <img className="chip-button-img" src={pokerChip} alt="chip button"/>
+        <div className="button-text"> {this.props.text}</div>
+      </div>
+    );
+  }
 }
 
 export default ChipButton;
