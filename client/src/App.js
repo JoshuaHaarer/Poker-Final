7a1c1bb81840b9c1fdb697107ff70b1d590ec71b
joshua <joshuahaarer86@gmail.com>
2018-07-12 18:58:14 -0700

    update RFID

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <RCard />
-        <Footer />
+        
       </div>
     );
   }
--- client/src/components/RFIDImageCard/RCard.js
+++ client/src/components/RFIDImageCard/RCard.js
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import "./RCard.css";
+import RFID2 from "../RFIDInfo/RFID2.json";
 
 
 
+class RCard extends Component {
+    state = {
+        potato: "./assets/images/playing_card_back.png",
+        potato2: "./assets/images/playing_card_back.png"
+      };
 
-const RCard = () => (
-    <div className="card" style={{ width: '18rem' }}>
-        <img className="card-img-top" src="" alt="Card image cap" />
-        <img className="card-img-top" src="" alt="Card image cap" />
-        <div className="card-body">
-            <h5 className="card-title">Your Cards</h5>
-            <p className="card-text"><div className="form-group">
-                <label for="exampleFormControlTextarea1">Example textarea</label>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="2"></textarea>
-            </div></p>
-            <a href="#" className="btn btn-primary">Verify Cards</a>
-        </div>
-    </div>
+    
 
-);
+    handleFormSubmit = () => {
+        
+        Object.keys(RFID2).forEach(function(x) {
+            console.log(document.getElementById("card1"));
+            console.log(document.getElementById("card2"));
+            if ((document.getElementById(("card1").trim())) === (document.getElementById(("card2").trim()))){
+                console.log("error");
+            }else{
+                console.log("this:", this);
+                console.log("RFID2:", RFID2);
+                RCard.bind(this);
+                console.log("new this:", this);
+            }          
+            console.log(x, RFID2[x].image);
+        });
+        this.setState({
+            potato: "Whoa",
+            potato2: "Yeah"
+        });
+    }
 
+    /*onChange() {
+        console.log(this.state);
+    }*/
+
+    render() {
+        return (
+            <div className="card" style={{ width: '18rem' }}>
+                <img className="card-img-top" src={this.state.potato} alt="" />
+                <img className="card-img-top" src={this.state.potato2} alt="" />
+                <div className="card-body">
+                    <h5 className="card-title">Your Cards</h5>
+                    <input className="form-control" id="card1" type="text" placeholder="input-1" />
+                    <input className="form-control" id="card2" type="text" placeholder="input-2" />
+                </div>
+                <a className="btn btn-primary" onClick={this.handleFormSubmit}>Verify Cards</a>
+            </div>
+            
+
+        );
+    }
+}
 export default RCard;
