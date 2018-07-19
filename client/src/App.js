4992e4d860961fb94f3fd8c3699869f33c5a5c1d
Zane P <ztpence@gmail.com>
2018-07-18 22:32:39 -0700

    working out some basic authentication logic on the front-end, including redirect

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import axios from "axios";
 import TitlePage from "./pages/TitlePage";
 import PokerTable from "./pages/PokerTable";
 import Footer from "./components/Footer";
@@ -10,23 +16,47 @@ import CreateGame from "./pages/CreateGame";
 import CardReader from "./pages/CardReader";
 import PokerCounter from "./pages/PokerCounter";
 
+const AuthRoute = ({ loggedIn, ...rest }) =>
+  loggedIn ? <Route {...rest} /> : <Redirect to="/login" />;
+
 class App extends Component {
+  state = {
+    loggedIn: false,
+    user: null
+  };
+
+  componentDidMount() {
+    // ajax call to get current user if exists
+    axios
+      .get("/api/users/current")
+      .then(response => {
+        console.log("resoponse", response);
+      })
+      .catch(error => {
+        console.log("error", error.response);
+      });
+  }
+
   render() {
+    const { loggedIn } = this.state;
+
     return (
       <Router>
         <div className="App">
           <Switch>
             <Route exact path="/" component={TitlePage} />
-            <Route exact path="/table" component={PokerTable} />
-            <Route exact path="/CreateGame" component={CreateGame} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/profile" component={Profile} />
+
+            {!loggedIn && <Route exact path="/login" component={Login} />}
+            {!loggedIn && <Route exact path="/register" component={Register} />}
+
+            <AuthRoute exact path="/table" component={PokerTable} />
+            <AuthRoute exact path="/CreateGame" component={CreateGame} />
+            <AuthRoute exact path="/createprofile" component={Profile} />
+            <AuthRoute exact path="/CardReader" component={CardReader} />
 
             {/*<Route exact path="/createprofile" component={CreateProfile}/>*/}
             {/*<Route exact path="/login" component={Login}/>*/}
             {/*<Route exact path="/CreateGame" component={CreateGame}/>*/}
-            <Route exact path="/CardReader" component={CardReader} />
           </Switch>
           <Footer />
         </div>
